d1f178842a79e72f91b32768ae6449b7a64c22b6
MSkiLLz <36935799+MSkiLLz@users.noreply.github.com>
2026-04-13 21:39:31 -0500

    Update train.py
    Week 7 Day 1: Introduce PyTorch tensors and autograd

--- notebooks/neural_network/train.py
+++ notebooks/neural_network/train.py
@@ -1,13 +1,21 @@
-import numpy as np
+import torch
 
-Input Data
-   ↓
-Forward Propagation
-   ↓
-Loss Function
-   ↓
-Backpropagation
-   ↓
-Optimizer (update weights)
-   ↓
-Repeat
+x = torch.tensor([1.0, 2.0, 3.0])
+print(x)
+
+X = torch.tensor([[1.0, 2.0],
+                  [3.0, 4.0]])
+                  
+w = torch.tensor([0.5, 0.2])
+
+z = torch.matmul(X, w)
+print(z)
+
+x = torch.tensor(2.0, requires_grad=True)
+
+y = x**2 + 3*x
+
+y.backward()
+
+print(x.grad)
+
