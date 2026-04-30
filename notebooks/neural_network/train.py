4ad0914ab4a80a989ef7ef27115fa0e1fa567f7f
MSkiLLz <36935799+MSkiLLz@users.noreply.github.com>
2026-04-29 20:43:57 -0500

    Update train.py
    Week 9 Day 3: Implement matrix factorization with embeddings

--- notebooks/neural_network/train.py
+++ notebooks/neural_network/train.py
@@ -1,31 +1,52 @@
 import torch
 
+num_users = 3
+num_items = 3
+embedding_dim = 2
+
+user_emb = torch.randn(num_users, embedding_dim, requires_grad=True)
+item_emb = torch.randn(num_items, embedding_dim, requires_grad=True)
+
+def predict(u, i):
+    return torch.dot(user_emb[u], item_emb[i])
+
+
 ratings = torch.tensor([
-    [5, 0, 3],  # User 1
-    [4, 0, 2],  # User 2
-    [0, 5, 4],  # User 3
+    [5, 0, 3],
+    [4, 0, 2],
+    [0, 5, 4]
 ], dtype=torch.float)
 
-user_profile = torch.tensor([1.0, 0.0])  # likes action
+loss = 0
 
-def similarity(u1, u2):
-    return torch.dot(u1, u2)
+for u in range(num_users):
+    for i in range(num_items):
+        if ratings[u, i] > 0:
+            pred = predict(u, i)
+            loss += (pred - ratings[u, i])**2
+            
 
-sim_1_2 = similarity(ratings[0], ratings[1])
-sim_1_3 = similarity(ratings[0], ratings[2])
+optimizer = torch.optim.SGD([user_emb, item_emb], lr=0.01)
 
-print(sim_1_2, sim_1_3)
+for epoch in range(100):
 
-target_user = ratings[0]
+    loss = 0
 
-scores = torch.zeros(3)
+    for u in range(num_users):
+        for i in range(num_items):
+            if ratings[u, i] > 0:
+                pred = predict(u, i)
+                loss += (pred - ratings[u, i])**2
 
-for i in range(len(ratings)):
-    if i != 0:
-        sim = similarity(target_user, ratings[i])
-        scores += sim * ratings[i]
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if epoch % 20 == 0:
+        print(loss.item())
+        
+user_id = 0
+
+scores = [predict(user_id, i).item() for i in range(num_items)]
 
 print(scores)
-
-
-
